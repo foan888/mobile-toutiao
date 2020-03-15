19416e728e1d0b432c4ab8a07ab81d2a0cc955ce
foan888 <foan888/outlook.com>
2020-03-15 23:09:42 +0800

    注册使用vant

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vant from 'vant'// 引入vant组件库
+import 'vant/lib/index.css'// 引入vant组件的样式
 
+Vue.use(Vant)// 注册Vant的所有组件 一旦注册  任意位置都可以使用
 Vue.config.productionTip = false
 
 new Vue({
